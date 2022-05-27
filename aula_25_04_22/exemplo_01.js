a3bb6800570d7e6795de15cea92268ea21781405
Leticia Honorio Pereira <79856171+Projetos-leticiahonorio@users.noreply.github.com>
2022-05-27 11:43:02 -0300

    Add files via upload

---
+++ aula_25_04_22/exemplo_01.js
@@ -0,0 +1,3 @@
+//Testando o plugin
+console.log('Testando o plugin')
+//aperte f8
---
+++ aula_25_04_22/exemplo_03.js
@@ -0,0 +1,10 @@
+var pais = 'EUA'  
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'BRA'){
+    console.log(`Voce é Brasileiro`)
+}
+else{
+    console.log(`Voce é Estrangeiro`)
+}
